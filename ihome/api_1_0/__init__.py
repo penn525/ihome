2374200c63db3127603cb1864d6793a48c2bd3f3
penn525 <pengwu525@163.com>
2019-10-30 15:15:43 +0800

    完成订单功能

--- ihome/api_1_0/__init__.py
+++ ihome/api_1_0/__init__.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 api = Blueprint('api_1_0', __name__)
 
 # 导入蓝图的视图
-from ihome.api_1_0 import demo, verify_code, passport, profile, houses
+from ihome.api_1_0 import demo, verify_code, passport, profile, houses, orders
